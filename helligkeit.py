c5ae2102bc29c029f57d0bf2292b3740d0425c91
domeix <github@domeix.de>
2018-05-05 22:44:40 +0200

    Praesi weiter

--- helligkeit.py
+++ helligkeit.py
@@ -1,11 +1,33 @@
+# Helligkeitsmessung auf Basis des Codes von pageauc (https://www.raspberrypi.org/forums/viewtopic.php?t=112888)
+
+import RPi.GPIO as GPIO
 import picamera
 import picamera.array
 import numpy as np
-with picamera.PiCamera() as camera:
-    camera.resolution = (100, 75)
-    with picamera.array.PiRGBArray(camera) as stream:
-        camera.exposure_mode = 'auto'
-        camera.awb_mode = 'auto'
-        camera.capture(stream, format='rgb')
-        pixAverage = int(np.average(stream.array[...,1]))
-print ("Light Meter pixAverage=%i" % pixAverage)
+import time
+
+
+GPIO.setmode(GPIO.BCM)		# GPIO ueber GPIO-Nummer ansprechen
+GPIO.setup(4, GPIO.OUT) 	# wir verwenden Pin Nummer 4
+
+for i in range(12):
+# zuerst LEDs ausschalten, um unverfaelschte Werte zu messen
+	GPIO.output(4, GPIO.LOW)	# ausschalten
+
+# dann durchschnittliche Helligkeit der Bildpunkte messen
+	with picamera.PiCamera() as camera:
+	    camera.resolution = (100, 75)
+	    with picamera.array.PiRGBArray(camera) as stream:
+	        camera.exposure_mode = 'auto'
+	        camera.awb_mode = 'auto'
+	        camera.capture(stream, format='rgb')
+	        pixAverage = int(np.average(stream.array[...,1]))
+
+	if pixAverage < 70:
+		GPIO.output(4, GPIO.HIGH)	# anschalten
+		print ("Anschalten weil: %i" % pixAverage)
+	else:
+		print ("aus lassen, weil %i" % pixAverage)
+
+
+	time.sleep(5)
